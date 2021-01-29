0846f1c12603f86f389132e3df0bfcc323b47441
pantor <lars.berscheid@online.de>
2021-01-29 08:35:49 +0100

    build benchmark in ci

--- test/otg-benchmark.cpp
+++ test/otg-benchmark.cpp
@@ -27,8 +27,8 @@ int main() {
 
     {
         constexpr size_t DOFs {7};
-        Ruckig<DOFs, true> otg {0.005};
-        // Reflexxes<DOFs> otg {0.005};
+        // Ruckig<DOFs, true> otg {0.005};
+        Reflexxes<DOFs> otg {0.005};
         InputParameter<DOFs> input;
         
         srand(43);
@@ -47,14 +47,14 @@ int main() {
             d.fill_or_zero(input.current_acceleration, 0.8);
             p.fill(input.target_position);
             d.fill_or_zero(input.target_velocity, 0.7);
-            d.fill_or_zero(input.target_acceleration, 0.6);
+            // d.fill_or_zero(input.target_acceleration, 0.6);
             l.fill(input.max_velocity, input.target_velocity);
             l.fill(input.max_acceleration, input.target_acceleration);
             l.fill(input.max_jerk);
 
-            if (!otg.validate_input(input)) {
-                continue;
-            }
+            // if (!otg.validate_input(input)) {
+            //     continue;
+            // }
 
             double time = check_calculation(otg, input);
             moving_average = moving_average + (time - moving_average) / n;
@@ -62,7 +62,7 @@ int main() {
             ++n;
         }
 
-        std::cout << "Benchmark for " << DOFs << " DoFs on " << number_trajectories << "trajectories" << << std::endl;
+        std::cout << "Benchmark for " << DOFs << " DoFs on " << number_trajectories << " trajectories" << std::endl;
         std::cout << "Average Calculation Duration " << moving_average << " [µs]" << std::endl;
         std::cout << "Worst Calculation Duration " << worst << " [µs]" << std::endl;
     }
