1522c1ee7a895616ee376c2140b13d7e962c7999
Lars Berscheid <lars.berscheid@kit.edu>
2021-02-08 16:41:18 +0100

    decrease required cmake to 3.10

--- cmake/FindReflexxes.cmake
+++ cmake/FindReflexxes.cmake
@@ -5,13 +5,15 @@ if(NOT TARGET Reflexxes::Reflexxes)
     elseif(UNIX)
       set(Reflexxes_LIB_DIR "${Reflexxes_ROOT_DIR}/Linux/x64/release/lib/shared")
     endif()
-    
+
     set(Reflexxes_INCLUDE_DIR "${Reflexxes_ROOT_DIR}/include")
   endif()
 
   find_library(Reflexxes ${REFLEXXES_TYPE} PATHS ${Reflexxes_LIB_DIR})
 
-  add_library(Reflexxes::Reflexxes INTERFACE IMPORTED)
-  target_include_directories(Reflexxes::Reflexxes INTERFACE ${Reflexxes_INCLUDE_DIR})
-  target_link_libraries(Reflexxes::Reflexxes INTERFACE ${Reflexxes})
+  if(Reflexxes)
+    add_library(Reflexxes::Reflexxes INTERFACE IMPORTED)
+    target_include_directories(Reflexxes::Reflexxes INTERFACE ${Reflexxes_INCLUDE_DIR})
+    target_link_libraries(Reflexxes::Reflexxes INTERFACE ${Reflexxes})
+  endif()
 endif()
