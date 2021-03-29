3835544cf9173543d810e13f2ab4c100fe6e6ad6
pantor <lars.berscheid@online.de>
2021-03-29 10:59:25 +0200

    add setup.py, version 0.2.1

---
+++ setup.py
@@ -0,0 +1,82 @@
+import os
+import re
+import subprocess
+
+from setuptools import setup, Extension, find_packages
+from setuptools.command.build_ext import build_ext
+from distutils.version import LooseVersion
+
+
+with open('README.md', 'r') as readme_file:
+    long_description = readme_file.read()
+
+
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=''):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
+
+
+class CMakeBuild(build_ext):
+    def run(self):
+        try:
+            out = subprocess.check_output(['cmake', '--version'])
+        except OSError as err:
+            raise RuntimeError(
+                'CMake must be installed to build the following extensions: ' +
+                ', '.join(e.name for e in self.extensions)
+            ) from err
+
+        cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)', out.decode()).group(1))
+        if cmake_version < LooseVersion('3.10.0'):
+            raise RuntimeError('CMake >= 3.10.0 is required')
+
+        for ext in self.extensions:
+            self.build_extension(ext)
+
+    def build_extension(self, ext):
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+
+        build_type = os.environ.get('BUILD_TYPE', 'Release')
+        build_args = ['--config', build_type]
+        build_args += ['--', '-j2']
+
+        # Pile all .so in one place and use $ORIGIN as RPATH
+        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir]
+        cmake_args += ['-DCMAKE_BUILD_WITH_INSTALL_RPATH=TRUE']
+        cmake_args += ['-DCMAKE_INSTALL_RPATH={}'.format('$ORIGIN')]
+        cmake_args += ['-DCMAKE_BUILD_TYPE=' + build_type]
+
+        env = os.environ.copy()
+        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
+                                                              self.distribution.get_version())
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
+        subprocess.check_call(['cmake', '--build', '.', '--target', ext.name] + build_args, cwd=self.build_temp)
+
+
+setup(
+    name='ruckig',
+    version='0.2.1',
+    description='Online Trajectory Generation. Real-time. Time-optimal. Jerk-constrained.',
+    description_file='README.md',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Lars Berscheid',
+    author_email='lars.berscheid@kit.edu',
+    url='https://github.com/pantor/ruckig',
+    packages=find_packages(),
+    license='MIT',
+    ext_modules=[CMakeExtension('ruckig')],
+    cmdclass=dict(build_ext=CMakeBuild),
+    keywords=['robotics', 'trajectory-generation', 'jerk-constrained', 'time-optimal'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: C++',
+    ],
+    python_requires='>=3.6',
+)
