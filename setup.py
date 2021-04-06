0b2741a55091f1f55fb8ccaa83a33076fbe522c6
Lars Berscheid <lars.berscheid@kit.edu>
2021-04-06 11:29:28 +0200

    use checkout@v2 in ci

--- setup.py
+++ setup.py
@@ -55,13 +55,9 @@ class CMakeBuild(build_ext):
             '-DCMAKE_POSITION_INDEPENDENT_CODE=ON',
         ]
 
-        # env = os.environ.copy()
-        # env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
-        #                                                       self.distribution.get_version())
-        
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
-        
+
         subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp)
         subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
 
