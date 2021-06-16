227c7ed56f0a343638138c360884f4fb9a4b6e4e
Lars Berscheid <lars.berscheid@kit.edu>
2021-06-16 17:30:23 +0200

    add patch script for C++11

---
+++ patch-c++11.sh
@@ -0,0 +1,13 @@
+mkdir patch-cpp11
+cp -r include src examples cmake test CMakeLists.txt patch-cpp11/
+
+cd patch-cpp11
+mkdir -p include/nonstd/
+
+# Download optional polyfill
+wget -O include/nonstd/optional.hpp https://raw.githubusercontent.com/martinmoene/optional-lite/master/include/nonstd/optional.hpp
+
+# Replace optional, if constexpr, and C++ version
+sed -i -e 's|std::optional|nonstd::optional|g' -e 's|<optional>|<nonstd/optional.hpp>|g' -e 's|if constexpr|if|g' include/ruckig/*.hpp
+sed -i -e 's|if constexpr|if|g' src/*.cpp
+sed -i -e 's|cxx_std_17|cxx_std_11|g' CMakeLists.txt
