7391d022726f9aac554d9403f0c436cb331b81e3
Lars Berscheid <lars.berscheid@kit.edu>
2021-06-18 09:31:07 +0200

    c++11 dont treat warnings as errors

--- patch-c++11.sh
+++ patch-c++11.sh
@@ -11,4 +11,4 @@ wget -O include/nonstd/optional.hpp https://raw.githubusercontent.com/martinmoen
 
 # Replace optional, if constexpr, and C++ version
 sed -i -e 's|std::optional|nonstd::optional|g' -e 's|<optional>|<nonstd/optional.hpp>|g' -e 's|if constexpr|if|g' include/ruckig/*.hpp
-sed -i -e 's|cxx_std_17|cxx_std_11|g' -e 's|if(BUILD_PYTHON_MODULE)|if(FALSE)|g' -e 's|if(BUILD_BENCHMARK)|if(FALSE)|g' CMakeLists.txt
+sed -i -e 's|cxx_std_17|cxx_std_11|g' -e 's|if(BUILD_PYTHON_MODULE)|if(FALSE)|g' -e 's|if(BUILD_BENCHMARK)|if(FALSE)|g' -e 's| /WX||g' -e 's| -Werror||g' CMakeLists.txt
