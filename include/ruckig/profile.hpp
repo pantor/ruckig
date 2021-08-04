30763be6a1c2586a3ef8fde4ebea34f1938aa7e6
Lars Berscheid <lars.berscheid@kit.edu>
2021-08-04 09:27:34 +0200

    in vel interface, change acc threshold

--- include/ruckig/profile.hpp
+++ include/ruckig/profile.hpp
@@ -86,11 +86,11 @@ struct Profile {
         // Velocity limit can be broken in the beginning if both initial velocity and acceleration are too high
         // std::cout << std::setprecision(15) << "target: " << std::abs(p[7]-pf) << " " << std::abs(v[7] - vf) << " " << std::abs(a[7] - af) << " T: " << t_sum[6] << " " << to_string() << std::endl;
         return std::abs(v[7] - vf) < 1e-8
-            && std::abs(a[7] - af) < 1e-12 // This is not really needed, but we want to double check
+            && std::abs(a[7] - af) < 1e-10 // This is not really needed, but we want to double check
             && a[1] >= aLowLim && a[3] >= aLowLim && a[5] >= aLowLim
             && a[1] <= aUppLim && a[3] <= aUppLim && a[5] <= aUppLim;
     }
-    
+
     template<JerkSigns jerk_signs, Limits limits>
     inline bool check_for_velocity_with_timing(double, double jf, double aMax, double aMin) {
         // Time doesn't need to be checked as every profile has a: tf - ... equation
