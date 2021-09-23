86992b04e4633a175441a7034636156c539d3911
pantor <lars.berscheid@online.de>
2021-09-23 11:18:09 +0200

    fix nullopt in c++11 patch

--- patch-c++11.sh
+++ patch-c++11.sh
@@ -11,6 +11,6 @@ wget -O include/nonstd/optional.hpp https://raw.githubusercontent.com/martinmoen
 wget -O include/nonstd/variant.hpp https://raw.githubusercontent.com/martinmoene/variant-lite/master/include/nonstd/variant.hpp
 
 # Replace optional, if constexpr, and C++ version
-sed -i -e 's|std::optional|nonstd::optional|g' -e 's|<optional>|<nonstd/optional.hpp>|g' -e 's|if constexpr|if|g' include/ruckig/*.hpp
+sed -i -e 's|std::optional|nonstd::optional|g' -e 's|std::nullopt|nonstd::nullopt|g' -e 's|<optional>|<nonstd/optional.hpp>|g' -e 's|if constexpr|if|g' include/ruckig/*.hpp
 sed -i -e 's|std::variant|nonstd::variant|g' -e 's|std::visit|nonstd::visit|g' -e 's|std::holds_alternative|nonstd::holds_alternative|g' -e 's|<variant>|<nonstd/variant.hpp>|g' include/ruckig/*.hpp
 sed -i -e 's|cxx_std_17|cxx_std_11|g' -e 's|if(BUILD_PYTHON_MODULE)|if(FALSE)|g' -e 's|if(BUILD_BENCHMARK)|if(FALSE)|g' -e 's| /WX||g' -e 's| -Werror||g' CMakeLists.txt
