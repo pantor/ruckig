095e9e6b7a4986856e2886e4a073d34ff23101f1
pantor <lars.berscheid@online.de>
2021-11-08 17:51:14 +0100

    fix c+11 patch

--- scripts/patch-c++11.sh
+++ scripts/patch-c++11.sh
@@ -10,6 +10,6 @@ mkdir -p include/nonstd/
 wget -O include/nonstd/optional.hpp https://raw.githubusercontent.com/martinmoene/optional-lite/master/include/nonstd/optional.hpp
 
 # Replace optional, if constexpr, and C++ version
-sed -i -e 's|std::optional|nonstd::optional|g' -e 's|<optional>|<nonstd/optional.hpp>|g' -e 's|if constexpr|if|g' include/ruckig/*.hpp
+sed -i -e 's|std::nullopt|nonstd::nullopt|g' -e 's|std::optional|nonstd::optional|g' -e 's|<optional>|<nonstd/optional.hpp>|g' -e 's|if constexpr|if|g' include/ruckig/*.hpp
 sed -i -e 's|std::nullopt|nonstd::nullopt|g' test/otg-*.cpp
 sed -i -e 's|cxx_std_17|cxx_std_11|g' -e 's|if(BUILD_PYTHON_MODULE)|if(FALSE)|g' -e 's|if(BUILD_BENCHMARK)|if(FALSE)|g' -e 's| /WX||g' -e 's| -Werror||g' CMakeLists.txt
