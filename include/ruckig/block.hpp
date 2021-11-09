a018df7749613f1218b750046ef37e5e158b0540
pantor <lars.berscheid@online.de>
2021-11-09 14:23:41 +0100

    optimize block class

--- include/ruckig/block.hpp
+++ include/ruckig/block.hpp
@@ -17,17 +17,26 @@ class Block {
         double left, right; // [s]
         Profile profile; // Profile corresponding to right (end) time
 
-        explicit Interval(double left, double right, const Profile& profile): left(left), right(right), profile(profile) { };
+        explicit Interval(const Profile& profile_left, const Profile& profile_right) {
+            const double left_duration = profile_left.t_sum[6] + profile_left.brake.duration;
+            const double right_duration = profile_right.t_sum[6] + profile_right.brake.duration;
+            if (left_duration < right_duration) {
+                left = left_duration;
+                right = right_duration;
+                profile = profile_right;
+            } else {
+                left = right_duration;
+                right = left_duration;
+                profile = profile_left;
+            }
+        };
     };
 
-    inline static void add_interval(std::optional<Block::Interval>& interval, const Profile& left, const Profile& right) {
-        const double left_duration = left.t_sum[6] + left.brake.duration;
-        const double right_duraction = right.t_sum[6] + right.brake.duration;
-        if (left_duration < right_duraction) {
-            interval = Block::Interval(left_duration, right_duraction, right);
-        } else {
-            interval = Block::Interval(right_duraction, left_duration, left);
-        }
+    void set_min_profile(const Profile& profile) {
+        p_min = profile;
+        t_min = p_min.t_sum[6] + p_min.brake.duration;
+        a = std::nullopt;
+        b = std::nullopt;
     }
 
     template<size_t N>
@@ -45,9 +54,6 @@ public:
     // Max. 2 intervals can be blocked: called a and b with corresponding profiles, order does not matter
     std::optional<Interval> a, b;
 
-    explicit Block() { }
-    explicit Block(const Profile& p_min): p_min(p_min), t_min(p_min.t_sum[6] + p_min.brake.duration) { }
-
     template<size_t N, bool numerical_robust = true>
     static bool calculate_block(Block& block, std::array<Profile, N>& valid_profiles, size_t valid_profile_counter) {
         // std::cout << "---\n " << valid_profile_counter << std::endl;
@@ -56,12 +62,12 @@ public:
         // }
 
         if (valid_profile_counter == 1) {
-            block = Block(valid_profiles[0]);
+            block.set_min_profile(valid_profiles[0]);
             return true;
 
         } else if (valid_profile_counter == 2) {
             if (std::abs(valid_profiles[0].t_sum[6] - valid_profiles[1].t_sum[6]) < 8*std::numeric_limits<double>::epsilon()) {
-                block = Block(valid_profiles[0]);
+                block.set_min_profile(valid_profiles[0]);
                 return true;
             }
 
@@ -69,8 +75,8 @@ public:
                 const size_t idx_min = (valid_profiles[0].t_sum[6] < valid_profiles[1].t_sum[6]) ? 0 : 1;
                 const size_t idx_else_1 = (idx_min + 1) % 2;
 
-                block = Block(valid_profiles[idx_min]);
-                Block::add_interval(block.a, valid_profiles[idx_min], valid_profiles[idx_else_1]);
+                block.set_min_profile(valid_profiles[idx_min]);
+                block.a = Interval(valid_profiles[idx_min], valid_profiles[idx_else_1]);
                 return true;
             }
 
@@ -95,13 +101,13 @@ public:
         const auto idx_min_it = std::min_element(valid_profiles.cbegin(), valid_profiles.cbegin() + valid_profile_counter, [](const Profile& a, const Profile& b) { return a.t_sum[6] < b.t_sum[6]; });
         const size_t idx_min = std::distance(valid_profiles.cbegin(), idx_min_it);
 
-        block = Block(valid_profiles[idx_min]);
+        block.set_min_profile(valid_profiles[idx_min]);
 
         if (valid_profile_counter == 3) {
             const size_t idx_else_1 = (idx_min + 1) % 3;
             const size_t idx_else_2 = (idx_min + 2) % 3;
 
-            Block::add_interval(block.a, valid_profiles[idx_else_1], valid_profiles[idx_else_2]);
+            block.a = Interval(valid_profiles[idx_else_1], valid_profiles[idx_else_2]);
             return true;
 
         } else if (valid_profile_counter == 5) {
@@ -111,11 +117,11 @@ public:
             const size_t idx_else_4 = (idx_min + 4) % 5;
 
             if (valid_profiles[idx_else_1].direction == valid_profiles[idx_else_2].direction) {
-                Block::add_interval(block.a, valid_profiles[idx_else_1], valid_profiles[idx_else_2]);
-                Block::add_interval(block.b, valid_profiles[idx_else_3], valid_profiles[idx_else_4]);
+                block.a = Interval(valid_profiles[idx_else_1], valid_profiles[idx_else_2]);
+                block.b = Interval(valid_profiles[idx_else_3], valid_profiles[idx_else_4]);
             } else {
-                Block::add_interval(block.a, valid_profiles[idx_else_1], valid_profiles[idx_else_4]);
-                Block::add_interval(block.b, valid_profiles[idx_else_2], valid_profiles[idx_else_3]);
+                block.a = Interval(valid_profiles[idx_else_1], valid_profiles[idx_else_4]);
+                block.b = Interval(valid_profiles[idx_else_2], valid_profiles[idx_else_3]);
             }
             return true;
         }
