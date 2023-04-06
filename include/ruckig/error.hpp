00fd36da9b43b521d3b4dfee5cae4f602faa191c
pantor <lars.berscheid@online.de>
2023-04-06 16:10:06 +0200

    add actual RuckigError definition

---
+++ include/ruckig/error.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <exception>
+#include <stdexcept>
+#include <string>
+
+
+namespace ruckig {
+
+struct RuckigError: public std::runtime_error {
+    explicit RuckigError(const std::string& message): std::runtime_error("\n[ruckig]\t" + message + "\n") {}
+};
+
+}
