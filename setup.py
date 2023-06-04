3fcf15f8441c6f1d62e87fd5be40a46094f7866f
pantor <lars.berscheid@online.de>
2023-06-04 08:50:50 -0700

    bump to v0.10.0

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name='ruckig',
-    version='0.9.3',
+    version='0.10.0',
     description='Instantaneous Motion Generation for Robots and Machines.',
     long_description=long_description,
     long_description_content_type='text/markdown',
