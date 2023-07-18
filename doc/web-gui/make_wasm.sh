bc4b29b82a7e5abdba1a7fb52b7c1faf409d8f7c
Berscheid <1885260+pantor@users.noreply.github.com>
2023-07-18 12:43:02 +0200

    Web-based GUI for Ruckig (#164)
    * Adding web-visualization for motion planning in the browser
    
    * update to wasm and modern web tooling
    
    ---------
    
    Co-authored-by: Joachim Schiele <joachim.schiele@teamspeak.com>

---
+++ doc/web-gui/make_wasm.sh
@@ -0,0 +1,7 @@
+emcc src/ruckig/wasm.cpp src/ruckig/brake.cpp \
+    src/ruckig/position-second-step1.cpp src/ruckig/position-second-step2.cpp src/ruckig/position-third-step1.cpp src/ruckig/position-third-step2.cpp \
+    src/ruckig/velocity-second-step1.cpp src/ruckig/velocity-second-step2.cpp src/ruckig/velocity-third-step1.cpp src/ruckig/velocity-third-step2.cpp \
+    -Iinclude -Ithird_party \
+    -std=c++17 -lembind -Os \
+    -s MODULARIZE -s EXPORT_ES6 -s EXPORT_NAME='RuckigModule' -s ENVIRONMENT='web' -s EXPORTED_RUNTIME_METHODS=ccall,cwrap \
+    -o doc/web-gui/src/ruckig.js
